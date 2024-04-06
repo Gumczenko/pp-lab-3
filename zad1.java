3339eca2a2d8989999c868643e43bd9c0b3400e5
Piotr Gumkowski <efscar123@gmail.com>
2024-04-06 18:39:50 +0200

    zdanie1

---
+++ zad1.java
@@ -0,0 +1,32 @@
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
+
+public class zad1 {
+    public static void main(String[] args) {
+        Point point = new Point(5, 3);
+        System.out.println(( point.getX() + ", " + point.getY()  ));
+    }
+}
