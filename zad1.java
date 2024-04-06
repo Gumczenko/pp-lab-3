a40292562b8cd307b641cb1cc20b36ee5b57753b
Piotr Gumkowski <efscar123@gmail.com>
2024-04-06 18:41:43 +0200

    zadanie1

--- zad1.java
+++ zad1.java
@@ -6,19 +6,15 @@ class Point {
         this.x = x;
         this.y = y;
     }
-
     public double getX() {
         return x;
     }
-
     public double getY() {
         return y;
     }
-
     public void setX(double x) {
         this.x = x;
     }
-
     public void setY(double y) {
         this.y = y;
     }
