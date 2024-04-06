aa8d0f43e4886c7646aafe4f34a2d122d3afba91
Piotr Gumkowski <efscar123@gmail.com>
2024-04-06 19:40:24 +0200

    modded

--- geometry/Main.java
+++ geometry/Main.java
@@ -1,16 +1,15 @@
 package geometry;
-
 public class Main {
     public static void main(String[] args) {
-        Point center = new Point(1, 2);
-        double radius = 4;
-        String color = "Red"; // Kolor dla przykładu
-
-        ColoredCircle coloredCircle = new ColoredCircle(center, radius, color);
-
-        System.out.println("Współrzędne środka: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
-        System.out.println("Promień: " + coloredCircle.getRadius());
-        System.out.println("Kolor: " + coloredCircle.getColor());
-        System.out.println("Obwód: " + coloredCircle.calculatePerimeter());
+        Circle[] circles = new Circle[2];
+        circles[0] = new Circle(new Point(1, 2), 3);
+        circles[1] = new ColoredCircle(new Point(3, 4), 5, "blue");
+        for (Circle circle : circles) {
+            System.out.println("pole powierzchni " + circle.calculateArea());
+            if (circle instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) circle;
+                System.out.println("kolor " + coloredCircle.getColor());
+            }
+        }
     }
 }
